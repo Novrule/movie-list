68df32ccb3011b3b8d1f6f48788a6bee6bd9807c
top-chaser <2000dnjswns@gmail.com>
2023-11-02 19:57:49 +0900

    fix : update path

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,19 +5,19 @@ import styles from "./Movie.module.css";
 const Movie = ({ coverImg, title, id, year, summary, genres }) => {
   return (
     <div className={styles.movie}>
-      <img src={coverImg} alt={title} className={styles.movie__img} />
-      <h2 className={styles.movie__title}>
-        <Link basename={process.env.PUBLIC_URL} to={`/movie/${id}`}>
-          {title}
-        </Link>
-      </h2>
-      <h3 className={styles.movie__year}>{year}</h3>
-      <p>{summary.length > 235 ? `${summary.slice(0, 232)}...` : summary}</p>
-      <ul className={styles.movie__genres}>
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
+      <img src={coverImg} alt={title} className={styles.movie_img} />
+      <div>
+        <h2 className={styles.movie_title}>
+          <Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie_year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 232)}...` : summary}</p>
+        <ul className={styles.movie_genres}>
+          {genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
